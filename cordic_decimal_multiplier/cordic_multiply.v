3a1fe7a125d3fff20d56fb2f0f2960748533d2e2
JasonHsiao8 <jasonhsiao2019@gmail.com>
2025-09-01 01:02:32 +0800

    Add files via upload

---
+++ cordic_decimal_multiplier/cordic_multiply.v
@@ -0,0 +1,81 @@
+module cordic_multiply #(parameter WL = 16, FL = 14, N_ITER = 15)(
+    input      clk,
+    input      rst_n,
+    input      start,
+    input      signed [WL-1:0] in1,    // Q1.14  [-π/2, π/2] angle_in
+    input      signed [WL-1:0] in2,    // Q1.14  cos_out
+    output reg signed [WL-1:0] out,    // Q1.14  sin_out
+    output reg done
+);
+
+    // ===== Registers Definition =====
+    reg signed [WL-1:0] x, z;
+    reg signed [WL:0] y;
+    reg [4:0] i;
+    reg [1:0] state, next_state;
+    reg [WL-1:0] two;
+
+    // ====== STATE Declaration ======
+    localparam IDLE = 2'd0, APPROX = 2'd1, DONE = 2'd2;
+
+    always @(posedge clk or negedge rst_n) begin
+        if(!rst_n)  state <= IDLE;
+        else        state <= next_state;
+    end
+
+    always @(*) begin
+    case (state)
+        IDLE    : next_state = (start) ? APPROX : IDLE;
+        APPROX  : next_state = (i == N_ITER-1) ? DONE : APPROX;
+        DONE    : next_state = IDLE;
+        default :;
+    endcase 
+    end
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            out <= 0;
+            i   <= 0;
+        end else begin
+            case (state)
+                IDLE: begin
+                    if (start) begin
+                        x <= in1;
+                        y <= 0;
+                        z <= in2;
+                        two <= 16'sb0100_0000_0000_0000;
+                        i <= 0;
+                    end
+                end
+
+                APPROX: begin
+                    if (z[WL-1] == 0) begin // positive 
+                        y <= y + (x >>> i);
+                        z <= z - (two >>> i);
+                        //state <= ((z - atan_table[i]) == 16'sd0) ? DONE : APPROX;
+                    end else begin 
+                        y <= y - (x >>> i);
+                        z <= z + (two >>> i);
+                        //state <= ((z + atan_table[i]) == 16'sd0) ? DONE : APPROX;
+                    end
+                    i <= i + 1;
+                end
+
+                DONE: begin
+                    out <= y[16:1];
+                end
+            endcase
+        end
+    end
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) done <= 0;
+        else begin
+            case (state)
+                IDLE  : done <= 0;
+                DONE  : done <= 1;
+                default:; 
+            endcase
+        end
+    end
+endmodule
---
+++ cordic_decimal_multiplier/cordic_multiply_tb.v
@@ -0,0 +1,105 @@
+`timescale 1ns/1ps
+
+module cordic_multiply_tb;
+
+    // ===== Parameter Definitions =====
+    parameter WL = 16;    // Word length
+    parameter FL = 14;    // Fraction length
+    parameter N_ITER = 15;
+
+    // ===== Testbench Signals =====
+    reg clk;
+    reg rst_n;
+    reg start;
+    reg  signed [WL-1:0] in1;  // Q1.14 格式，[-π/2, π/2]
+    reg  signed [WL-1:0] in2;   // Q1.14 格式
+    wire signed [WL-1:0] out;   // Q1.14 格式
+    wire done;
+
+    // ===== Instantiate the CORDIC Module =====
+    cordic_multiply #(WL, FL, N_ITER) uut (
+        .clk(clk),
+        .rst_n(rst_n),
+        .start(start),
+        .in1(in1),
+        .in2(in2),
+        .out(out),
+        .done(done)
+    );
+
+    // ===== Clock Generation =====
+    always begin
+        #5 clk = ~clk;  // 100 MHz clock
+    end
+
+    // ===== Testbench Procedure =====
+    initial begin
+        // Initialize signals
+        clk = 0;
+        rst_n = 1;
+        start = 0;
+        in1 = 16'sd0;
+        in2 = 16'sd0;
+
+        // Reset the DUT
+        rst_n = 0;
+        #10 rst_n = 1;
+        
+        // Test Case 1: 0 * 0.75
+        in1 = 16'sd0;  // 0 in Q1.14 format
+        in2 = 16'sb0011_0000_0000_0000;
+        start = 1;
+        #10 start = 0;
+        
+        // Wait for the calculation to finish
+        wait(done);
+        $display("Test 1 : 0 * 0.75 = %d", out);
+
+        // Test Case 2: 0.5 * 0.5
+        in1 = 16'sb0010_0000_0000_0000;
+        in2 = 16'sb0010_0000_0000_0000; 
+        #10 start = 1;
+        #10 start = 0;
+        
+        // Wait for the calculation to finish
+        wait(done);
+        $display("Test 2 : 0.5 * 0.5 = %d", out);
+        
+        // Test Case 3: -0.5 * 0.5
+
+        in1 =  16'sb0010_0000_0000_0000;
+        in2 = -16'sb0010_0000_0000_0000; 
+        #10 start = 1;
+        #10 start = 0;
+        
+        // Wait for the calculation to finish
+        wait(done);
+        $display("Test 3 : -0.5 * 0.5 = %d", out);
+
+        // Test Case 4: 1.9999 * 1.9999
+        
+        in1 = 16'sb0111_1111_1111_1111;
+        in2 = 16'sb0111_1111_1111_1111;  
+        #10 start = 1;
+        #10 start = 0;
+        
+        // Wait for the calculation to finish
+        wait(done);
+        $display("Test 4 : 1.9999 * 1.9999 = %d", out);
+        
+        // Test Case 5: 1.9999 * -1.9999
+        in1 =  16'sb0111_1111_1111_1111;
+        in2 = -16'sb0111_1111_1111_1111;  
+        #10 start = 1;
+        #10 start = 0;
+        
+        // Wait for the calculation to finish
+        wait(done);
+        $display("Test 5 : 1.9999 * -1.9999 = %d", out);
+
+        // End the simulation
+        #10
+        $finish;
+    end
+
+endmodule
